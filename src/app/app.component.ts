05031dd3e07f2fa549db25fef3d46eec80f58a8b
Sami alaa al sudani <samialaaalsudani@Samis-MacBook-Pro.local>
2021-08-13 18:13:05 -0400

    password generators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,51 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angularApp';
+  password='';
+  length = 0 ;
+  changeletter= false;
+  changenumber= false;
+  changesymbol= false;
+
+  onChangelength(value : string){
+    const parsedvalue = parseInt(value);
+    if(!isNaN(parsedvalue))
+    this.length = parseInt(value);
+  }
+
+
+  changeletters(){
+    this.changeletter = !this.changeletter;
+  }
+
+  changenumbers(){
+    this.changenumber = !this.changenumber;
+  }
+
+  changesymbols(){
+    this.changesymbol = !this.changesymbol;
+  }
+
+  onButtonClick(){
+   const numbers = "1234567890"
+   const letters = "abcdefghijklmnopqrstuvwxyz"
+   const symbols = "!@#$%^&*()?"
+   let validchars =""
+   if(this.changeletter){
+     validchars += letters;
+   }
+   if(this.changenumber){
+      validchars+= numbers;
+   }
+   if(this.changesymbol){
+     validchars+= symbols;
+   }
+
+   let generatedpassword = "";
+   for(let i = 0 ; i < this.length ; i++ ){
+     const index = Math.floor(Math.random() * validchars.length);
+     generatedpassword += validchars[index];
+   }
+   this.password = generatedpassword;
+  }
 }
